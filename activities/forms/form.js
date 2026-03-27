5b5d2e069c6aa7b88db996012481792f42a3d19c
Lester Vilo <lestervilo02@gmail.com>
2026-03-27 11:29:34 -1000

    ajax

---
+++ activities/forms/form.js
@@ -0,0 +1,46 @@
+<script>
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const nameInput = document.getElementById("name").value;
+        const emailInput = document.getElementById("email").value;
+        const passwordInput = document.getElementById("password").value;
+        const age = document.getElementById("age").value;
+
+        if (nameInput === "" || emailInput === "" || passwordInput === "") {
+            alert("Please fill in all required fields.");
+            return;
+        } else {
+            alert("Form submitted successfully!");
+        }
+
+        if (!age || age <18) {
+            alert("You must be at least 18 years old to submit this form.");
+            return;
+        }
+
+        const formData = {
+            name: nameInput,
+            email: emailInput,
+            password: passwordInput
+        };
+
+        console.log(formData);
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "submit.json", true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                alert("Form submitted successfully!");
+                const response = JSON.parse(xhr.responseText);
+                console.log("Response:", response);
+                document.getElementById("myForm").innerHTML = "";
+                document.getElementById("message").innerText = response.message;
+            }
+            else if (xhr.readyState === 4) {
+                alert("Error submitting form.");
+            }
+        };
+        xhr.send(JSON.stringify(formData));
+    });
+</script>
